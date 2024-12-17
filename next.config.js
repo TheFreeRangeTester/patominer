10a8929b15aa4596f86daa5f12b83872ed2f5936
Pato Miner <patominer@gmail.com>
2024-12-18 11:55:43 +1300

    Cambios

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
     env: {
         CONVERTKIT_FORM_ID: '7383840',
-        CONVERTKIT_API_KEY: 'tu-api-key'
+        CONVERTKIT_API_KEY: 'OkPhSKjNl04tilbYMuSU-C29gdXdTMAHF3QRyUu09_c'
     }
 }
 
