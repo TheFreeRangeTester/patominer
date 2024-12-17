d9d0e72ab0377644b798e248f8a9af82cdc3dbd0
Pato Miner <patominer@gmail.com>
2024-12-18 11:39:19 +1300

    Modificando

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+        CONVERTKIT_FORM_ID: '7383840',
+        CONVERTKIT_API_KEY: 'tu-api-key'
+    }
+}
+
+module.exports = nextConfig 
